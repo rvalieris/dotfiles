80ae95eabcfb2395db487ffec74d89dfcbf3d746
rvalieris <rvalieris@users.noreply.github.com>
2018-10-17 11:28:57 -0300

    use xsv for cl

--- shell/.shell/aliases.sh
+++ shell/.shell/aliases.sh
@@ -10,7 +10,7 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 alias grep='grep --color=auto'
-alias cl="column -t -s '	'"
+alias cl="xsv table -d'\t'"
 alias ghm='ghc --make -O3 -outputdir=/tmp/ -fforce-recomp'
 alias ghi='ghci +RTS -M1G -RTS'
 alias R='R --quiet --no-save'
