781409c3f4ca301c2df6d714256932c181823105
rvalieris <rvalieris@users.noreply.github.com>
2019-11-22 15:33:12 -0300

    only execute less_colors when interactive

--- shell/.shell/less_colors.sh
+++ shell/.shell/less_colors.sh
@@ -1,12 +1,15 @@
 # less termcap colors
 # https://unix.stackexchange.com/a/108840
 # man 5 terminfo
-export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # bold
-export LESS_TERMCAP_so=$(tput setaf 0; tput setab 7) # standout
-export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # underline
-export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
-export LESS_TERMCAP_me=$(tput sgr0)
+
+if [[ $- == *i* ]]; then # is interactive
+	export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # bold
+	export LESS_TERMCAP_so=$(tput setaf 0; tput setab 7) # standout
+	export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+	export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # underline
+	export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+	export LESS_TERMCAP_me=$(tput sgr0)
+fi
 #export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # blink
 #export LESS_TERMCAP_mr=$(tput rev) # reverse
 #export LESS_TERMCAP_mh=$(tput dim) # half-bright
