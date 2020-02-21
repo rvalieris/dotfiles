9d33c69e5040b062ae7fe2d5ce2c953180242fb1
rvalieris <rvalieris@users.noreply.github.com>
2020-02-21 15:37:53 -0300

    convert i3 config to j2 template

--- .config/i3/auto-floating.py
+++ .config/i3/auto-floating.py
@@ -2,26 +2,43 @@
 import os
 import shelve
 import i3ipc
+import subprocess
 
-i3 = i3ipc.Connection()
+i3 = i3ipc.Connection(auto_reconnect=True)
 cache = shelve.open(os.path.expanduser('~/.cache/i3-auto-floating'))
+config = os.path.expanduser("~/.cache/i3-auto-floating.config")
+
+def get_key(container):
+	c = container.window_class
+	if c is None: c = ""
+	i = container.window_instance
+	if i is None: i = ""
+	return '\x00'.join([c,i])
+
+def split_key(key):
+	return key.split('\x00')
+
+def write_config():
+	fd = open(config, 'w')
+	for k in cache.keys():
+		c, i = split_key(k)
+		fd.write('for_window [class="^{}$" instance="^{}$"] floating enable\n'.format(c,i))
+	fd.close()
+	subprocess.run(['yadm','alt'])
+	subprocess.run(['i3-msg','-q','reload'])
 
 def on_window(i3, event):
-	if event.change == 'new':
-		# new window apply floating if any
-		m = event.container.window_class + '|' + event.container.window_instance
-		if m in cache:
-			if cache[m]: event.container.command('floating enable')
-			else: event.container.command('floating disable')
-			#print(event.change, m, cache[m])
-		#else:
-		#	print(event.change, m)
-	elif event.change == 'close':
-		# closed window save floating mode
-		m = event.container.window_class + '|' + event.container.window_instance
-		cache[m] = event.container.floating == 'user_on'
-		cache.sync()
-		#print(event.change, m, cache[m])
+	if event.change == 'close':
+		k = get_key(event.container)
+		if event.container.floating == 'user_on' and k not in cache:
+			cache[k] = True
+			cache.sync()
+			write_config()
+		if event.container.floating == 'user_off' and k in cache:
+			del cache[k]
+			cache.sync()
+			write_config()
 
+write_config()
 i3.on(i3ipc.Event.WINDOW, on_window)
 i3.main()
