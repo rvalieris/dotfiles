2c0a760664fc829e2b826735d7db75f0c19f372f
rvalieris <rvalieris@users.noreply.github.com>
2022-03-14 13:41:10 -0300

    etc

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,4 +2,10 @@ with import <nixpkgs> {};
 {
   allowUnfree = true;
 
+  packageOverrides = pkgs: {
+    # https://github.com/NixOS/nixpkgs/issues/124965
+    rofi = pkgs.rofi.overrideAttrs (oldAttrs: rec {
+      buildInputs = builtins.filter (x: x.pname != "gdk-pixbuf") oldAttrs.buildInputs;
+    } );
+  };
 }
