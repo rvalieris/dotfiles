edab44c98e51233ea7c20385d60fe17f7659963b
rvalieris <rvalieris@users.noreply.github.com>
2025-07-02 18:02:12 -0300

    etc

--- .config/shell/less_colors.sh
+++ .config/shell/less_colors.sh
@@ -6,9 +6,11 @@ if [[ $- == *i* ]]; then # is interactive
 	export LESS_TERMCAP_md=$(tput bold; tput setaf 6) # bold
 	export LESS_TERMCAP_so=$(tput setaf 0; tput setab 7) # standout
 	export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-	export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # underline
+	export LESS_TERMCAP_us=$(tput bold; tput setaf 2)
 	export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
 	export LESS_TERMCAP_me=$(tput sgr0)
+	# https://bbs.archlinux.org/viewtopic.php?pid=2113876#p2113876
+	export MANROFFOPT=-c
 fi
 #export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # blink
 #export LESS_TERMCAP_mr=$(tput rev) # reverse
@@ -17,3 +19,4 @@ fi
 #export LESS_TERMCAP_ZV=$(tput rsubm)
 #export LESS_TERMCAP_ZO=$(tput ssupm) # superscript
 #export LESS_TERMCAP_ZW=$(tput rsupm)
+
